d2e638ebce22f585eba42a2c849fb72b3bc4e763
Willian  Candido da Silva <williancandidodasilva@Willians-Mac-mini.local>
2020-01-28 16:00:14 -0300

    reated NewsTableViewCell with custom layout

---
+++ Social/NewsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsTableViewCell.swift
+//  Social
+//
+//  Created by Willian  Candido da Silva on 27/01/20.
+//  Copyright © 2020 Guest User. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
